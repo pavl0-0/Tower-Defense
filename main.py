230a5b10b5e19ebb3c4b0754a0cb9798604a99b5
pavl0-0 <pavlo.zhuk@nure.ua>
2025-09-30 23:25:38 +0300

    Added enemy's movement by waypoints

--- main.py
+++ main.py
@@ -10,11 +10,18 @@ clock = pg.time.Clock()
 screen = pg.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
 pg.display.set_caption("Tower Defense")
 
-enemy_image = pg.image.load('assets/images/enemies/Enemy.png').convert_alpha()
+enemy_image = pg.image.load('assets/images/enemies/Enemy1.png').convert_alpha()
 
 enemy_group = pg.sprite.Group()
 
-enemy = Enemy((200, 300), enemy_image) 
+waypoints = [
+    (100, 100),
+    (400, 200),
+    (400, 100),
+    (200, 300)
+]
+
+enemy = Enemy(waypoints, enemy_image) 
 enemy_group.add(enemy)
 
 run = True
@@ -22,6 +29,12 @@ while run:
 
     clock.tick(c.FPS)
 
+    screen.fill("grey100")
+
+    pg.draw.lines(screen, "grey0", False, waypoints)
+
+    enemy_group.update()
+ 
     enemy_group.draw(screen)
 
     for event in pg.event.get():
